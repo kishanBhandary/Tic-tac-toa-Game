383bc775587e5cda32bec0201c7ec1c677da5a0c
kishanBhandary <kishanbhandary0@gmail.com>
2025-08-30 19:49:00 +0530

    add code

--- src/TicTacToeGUI.java
+++ src/TicTacToeGUI.java
@@ -9,7 +9,7 @@ public class TicTacToeGUI extends JFrame implements ActionListener {
     private boolean gameEnded = false;
 
     public TicTacToeGUI() {
-        setTitle("Tic-Tac-Toe Gameeeeeeeeeerrfeee");
+        setTitle("Tic-Tac-Toe Gameeeeeeeeeerrfeeee");
         setSize(500, 500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new GridLayout(3, 3));
